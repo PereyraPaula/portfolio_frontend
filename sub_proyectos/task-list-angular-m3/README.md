0693cfacb02bc8a72c2451f14df9459431ddff0c
PereyraPaula <paulapereyrallorens@gmail.com>
2022-09-27 18:51:00 -0300

    Proyecto practica TaskList - modulo 3

---
+++ sub_proyectos/task-list-angular-m3/README.md
@@ -0,0 +1,8 @@
+# TaskListAngular
+
+Proyecto básico realizado para uno de los modulos de argentina programa.
+El objetivo es aplicar lo aprendido.
+
+Paquetes usados:
+* json-server
+* font-awesome
---
+++ sub_proyectos/task-list-angular-m3/src/app/mock_task.ts
@@ -0,0 +1,29 @@
+import {Task} from "./Task"
+export const TASKS:Task[]  = [
+	{
+		id: 6,
+		text: "sed magna at",
+		day: "5/8/2022",
+		reminder: false,
+	}, {
+		id: 7,
+		text: "in faucibus orci",
+		day: "6/11/2022",
+		reminder: true,
+	}, {
+		id: 8,
+		text: "vestibulum sagittis sapien cum",
+		day: "12/17/2021",
+		reminder: true,
+	}, {
+		id: 9,
+		text: "vivamus tortor duis mattis",
+		day: "12/16/2021",
+		reminder: true,
+	}, {
+		id: 10,
+		text: "amet sem fusce consequat",
+		day: "3/17/2022",
+		reminder: true,
+	}
+]
